447294461774edf81ae4ab2a5c76b760360747be
shust-veronika <sustv153@gmail.com>
2026-01-10 21:32:44 +0000

    Deploying to gh-pages from @ shust-veronika/goit-js-hw-09@11e38f62dd97d3cc481d4f68ee08569f4dc470cd 🚀

---
+++ 1-gallery.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */import{S as t}from"./assets/vendor-CgTBfC_f.js";new t(".gallery a",{captions:!0,captionsData:"alt",captionPosition:"bottom",captionDelay:250});
+//# sourceMappingURL=1-gallery.js.map
