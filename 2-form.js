10238e5347f513436f7fe85e55f8daff06d852fc
shust-veronika <sustv153@gmail.com>
2026-01-11 18:41:40 +0000

    Deploying to gh-pages from @ shust-veronika/goit-js-hw-09@ad8f332675930a0fef02883f88caceff8955e98d 🚀

---
+++ 2-form.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */const t=document.querySelector(".feedback-form"),o="goit-example-message";t.addEventListener("input",e=>{localStorage.setItem(o,e.target.value)});t.addEventListener("submit",e=>{e.preventDefault(),console.log(e.target.elements.message.value),t.reset()});
+//# sourceMappingURL=2-form.js.map
